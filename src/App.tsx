b7f10e8eafffaa6c381bb6048696d3bd92e0ff8d
Kedarnath Peraka <kedarnathperaka@gmail.com>
2025-06-14 22:16:01 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import { Button } from 'primereact/button';
+
+
+function App() {
+
+  return (
+    <>
+    <Button label="Submit" />
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { PrimeReactProvider } from 'primereact/api';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
+createRoot(document.getElementById('root')!).render(
+
+    <PrimeReactProvider><App /></PrimeReactProvider>
+ 
+)
